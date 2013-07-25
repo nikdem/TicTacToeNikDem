277d54fb41fc1a158ba29a626ee9d23e9fd80b9e
nikdem <demidovez93@yandex.by>
2013-07-25 12:10:26 +0300

    Create Gamer.java

---
+++ src/Gamer.java
@@ -0,0 +1,15 @@
+
+public class Gamer {
+  private String name;
+	private int orOX;
+	Gamer(String name, int orOX) {
+		this.name = name;
+		this.orOX = orOX;
+	}
+	public String getName(){
+		return name;
+	}
+	public int getOrOX(){
+		return orOX;
+	}
+}
