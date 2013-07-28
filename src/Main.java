b3a6e6112beac3cb49a592c0b4bd7d41b18547a0
Николай <demidovez93@yandex.by>
2013-07-28 22:10:39 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,22 @@
 import java.io.PrintStream;
 
 public class Main {
-  public static void main(String[] args) {
-        try {
-            System.setOut(new PrintStream(System.out, true, "UTF-8"));
-        } catch (java.io.UnsupportedEncodingException ex) {
-            System.err.println("Unsupported encoding set for console");
-        }
-
+	public static void main(String[] args) {
 		System.out.println("Привет! Это игра \"Крестики-Нолики\"");
 
-        Gamer gamerX = new Gamer('X');
-        Gamer gamerO = new Gamer('O');
+        	Gamer gamerX = new Gamer('X');
+        	Gamer gamerO = new Gamer('O');
 		
 		Board board = new Board();
 		board.showBoard();
 		
 		Game game = new Game();
 		for(int countStep = 1; ; countStep++) {
-            game.stepPlayer(gamerX, board, 'x', countStep);
-            if(game.getQuit()) break;
+        	    game.stepPlayer(gamerX, board, 'x', countStep);
+        	    if(game.getQuit()) break;
 
-            game.stepPlayer(gamerO, board, 'o', countStep);
-            if(game.getQuit()) break;
+        	    game.stepPlayer(gamerO, board, 'o', countStep);
+         	   if(game.getQuit()) break;
 		}
-	}
+  	}
 }
