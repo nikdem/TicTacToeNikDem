e76460c90d20cd3b194e7d1c87bf7cd50d4359f3
nikdem <demidov93@mail.ru>
2013-07-28 17:41:26 +0300

    кодировка консоли

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.io.PrintStream;
+
 public class Main {
   public static void main(String[] args) {
+        try {
+            System.setOut(new PrintStream(System.out, true, System.getProperty("file.encoding")));
+        } catch (java.io.UnsupportedEncodingException ex) {
+            System.err.println("Unsupported encoding set for console: ");
+        }
+
+        System.out.println(System.getProperty("console.encoding"));
+        System.out.println(System.getProperty("file.encoding"));
 		System.out.println("Привет! Это игра \"Крестики-Нолики\"");
 
         Gamer gamerX = new Gamer('X');
